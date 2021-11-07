bb9825e84bbb0e45051d8e488c61975c357825cf
Sofronov Semyon <sofronov.semyon@gmail.com>
2021-11-07 21:51:03 +0400

    detach exercise section from main page

---
+++ src/components/ExerciseSection/ExerciseSection.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Button from '../ui/Button';
+import Badge from '../ui/Badge';
+import IndicatorBadge from '../ui/IndicatorBadge';
+
+import './index.scss';
+
+const ExerciseSection = () => {
+  return (
+    <section className="exercise-section">
+      <div className="exercise-section_left-column">
+        <Badge
+          type="new"
+          color="primary"
+          description="High Intensity workout to burn calories"
+          className="badge"
+        />
+        <h2 className="exercise-section_title">
+          Cardio
+          Exercise
+        </h2>
+        <p className="exercise-section_description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna aliqua.
+        </p>
+        <div className="exercise-section_buttons">
+          <Button
+            buttonText="Get Started"
+            color="primary"
+          />
+          <Button buttonText="Preview"/>
+        </div>
+      </div>
+      <div className="exercise-section_right-column">
+        <IndicatorBadge
+          value="38:14"
+          description="TIME"
+          color="yellow"
+        />
+        <IndicatorBadge
+          value="165"
+          description="EST CALORIES"
+          color="red"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default ExerciseSection;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,53 +1,14 @@
 import React from 'react';
-import Button from '../../components/ui/Button';
-
-import './index.scss';
-import IndicatorBadge from '../../components/ui/IndicatorBadge';
-import Badge from '../../components/ui/Badge';
 import PopularExercisesSection from '../../components/PopularExercisesSection';
 import WorkoutSection from '../../components/WorkoutSection';
+import ExerciseSection from '../../components/ExerciseSection';
+
+import './index.scss';
 
 const MainPage = () => {
   return (
     <div className="main-page_wrapper">
-      <section className="main-page_exercise">
-        <div className="main-page_left-column">
-          <Badge
-            type="new"
-            color="primary"
-            description="High Intensity workout to burn calories"
-            className="main-page_badge"
-          />
-          <h2 className="main-page_title">
-            Cardio
-            Exercise
-          </h2>
-          <p className="main-page_description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua.
-          </p>
-          <div className="main-page_buttons">
-            <Button
-              buttonText="Get Started"
-              color="primary"
-            />
-            <Button buttonText="Preview"/>
-          </div>
-        </div>
-        <div className="main-page_right-column">
-          <IndicatorBadge
-            value="38:14"
-            description="TIME"
-            color="yellow"
-          />
-          <IndicatorBadge
-            value="165"
-            description="EST CALORIES"
-            color="red"
-          />
-        </div>
-
-      </section>
+      <ExerciseSection/>
       <PopularExercisesSection/>
       <WorkoutSection/>
     </div>
